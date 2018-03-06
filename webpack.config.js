99bb29642bf2afc9d9c40605b11b2568c8e06f90
Tom Canham <tom@arcadianproductions.com>
2018-03-06 14:11:02 -0800

    Initial commit; setting up webpack skeleton

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  // Since 'aws-sdk' is not compatible with webpack,
+  // we exclude all node dependencies
+  externals: [nodeExternals()],
+  // Run babel on all .js files and skip those in node_modules
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        include: __dirname,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
